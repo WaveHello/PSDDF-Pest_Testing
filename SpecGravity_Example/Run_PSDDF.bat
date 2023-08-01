c4a6e72cf3d00e29f76d8dfa48abd5083e45f76f
Moore <1623971267121002@mil>
2023-08-01 09:43:27 -0500

    Minor Pest updates. Creation of plotting code
    Plot_PSDDF contains a class to hold PSDDF output files and functions to plot settlement and effective stress associated data.

--- SpecGravity_Example/Run_PSDDF.bat
+++ SpecGravity_Example/Run_PSDDF.bat
@@ -7,7 +7,4 @@ rem !!!! Important: Make sure the text file that the name is being read from has
 rem The format of the "InputFileName".text should be the name then an enter so the cursor goes to the start of the next line
 rem This is necessary as it acts as an enter in the PSDDF fortran code
 
-rem Clean up temporary files
-rem del input.txt
-
 @Pause
